dbede7d95e1c5809c646cd368cd26ba2b6b39bd4
grace <samson.grace.521@gmail.com>
2018-01-12 10:42:01 +0800

    test
    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var linebot = require('linebot');
 var express = require('express');
 
 var jQuery = require('jquery')
-//var jsdom = require("jsdom");
+var jsdom = require("jsdom");
 
 var bot = linebot({
   channelId: '1553329312',
@@ -10,14 +10,14 @@ var bot = linebot({
   channelAccessToken: 'ZHcGSnnInWxVJkwWjz2TEQ4Rmu7GFIZ82nqK/nnPckbR1zw9z0anx90lCndweFGfOalYMXdtp4DW7CUJrtZ3HpSTwf6osEKNCrBdY2muaHYUR8Dq8skykzIAQbmea2pMPRXC7eTa6vIjJoDcP3nd8AdB04t89/1O/w1cDnyilFU='
 });
 
-//require("jsdom").env("", function(err, window) {
-//    if (err) {
-//        console.error(err);
-//        return;
-//    }
-// 
-//    var $ = require("jquery")(window);
-//});
+require("jsdom").env("", function(err, window) {
+    if (err) {
+        console.error(err);
+        return;
+    }
+ 
+    var $ = require("jquery")(window);
+});
 
 var timer;
 var pm = [];
